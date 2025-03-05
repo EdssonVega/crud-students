f149ceb4b17b4e1bd5225105e167c7035aac2db0
Edsson <e_d_sx2@hotmail.com>
2025-03-05 17:52:08 -0400

    added function to set all the inputs

--- client/src/components/form/addStudent.jsx
+++ client/src/components/form/addStudent.jsx
@@ -8,7 +8,14 @@ export const AddStudent = () => {
     const [school, setSchool] = useState(0)
     const [cellphone, setCellphone] = useState(0)
 
-    const registerStudent = () => {
+    const setInfo = (infoType,event) => {
+        infoType(event.target.value)
+    }
+
+    const registerStudent = (e) => {
+        e.preventDefault()
+        console.log(`${name},${gender},${school},${cellphone}`)
+        /*
         Axios.post("http://localhost:3001/create",{
             name:name,
             gender:gender,
@@ -16,19 +23,20 @@ export const AddStudent = () => {
             cellphone:cellphone
         }).then(()=>{
             alert("student registered!");
-        })
+        })*/
     }
+
 
   return (
     <form onSubmit={registerStudent} className="addStudentContainer">
         <label htmlFor="name"> Name:</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" onChange={(event)=> setInfo(setName,event)}/>
         <label htmlFor="gender"> Gender:</label>
-        <input type="text" name="gender" id="gender"/>
+        <input type="text" name="gender" id="gender" onChange={(event)=> setInfo(setGender,event)}/>
         <label htmlFor="school"> School:</label>
-        <input type="number" name="school" id="dni"/>
+        <input type="text" name="school" id="school" onChange={(event)=> setInfo(setSchool,event)}/>
         <label htmlFor="cellphone"> Cellphone:</label>
-        <input type="number" name="cellphone" id="cellphone"/>
+        <input type="number" name="cellphone" id="cellphone" onChange={(event)=> setInfo(setCellphone,event)}/>
         <button type='submit'>Add student</button>
     </form>
   )
