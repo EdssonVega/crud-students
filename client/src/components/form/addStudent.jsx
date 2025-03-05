a7c428b5a997a66fd1bef4ef64c3e8707338ae23
Edsson <e_d_sx2@hotmail.com>
2025-03-05 17:27:45 -0400

    added connection to the data base

--- client/src/components/form/addStudent.jsx
+++ client/src/components/form/addStudent.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./addStudentStyles.css"
 
 export const AddStudent = () => {
+    const [name, setName] = useState("")
+    const [gender, setGender] = useState("")
+    const [school, setSchool] = useState(0)
+    const [cellphone, setCellphone] = useState(0)
+
   return (
     <form action="" className="addStudentContainer">
         <label htmlFor="name"> Name:</label>
         <input type="text" name="name" id="name" />
         <label htmlFor="gender"> Gender:</label>
         <input type="text" name="gender" id="gender"/>
-        <label htmlFor="dni"> Identification document number:</label>
-        <input type="number" name="dni" id="dni"/>
+        <label htmlFor="school"> School:</label>
+        <input type="number" name="school" id="dni"/>
         <label htmlFor="cellphone"> Cellphone:</label>
         <input type="number" name="cellphone" id="cellphone"/>
         <button type='submit'>Add student</button>
--- server/index.js
+++ server/index.js
@@ -1,5 +1,29 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"students"
+})
+
+app.post("/create",(req,res)=>{
+    const name = req.body.name;
+    const gender = req.body.gender;
+    const school = req.body.school;
+    const cellphone = req.body.cellphone;
+
+    db.query("INSERT INTO students(name,gender,school,cellphone) VALUES(?,?,?,?,?)",[name,gender,school,cellphone],
+    (err,result)=>{
+        if (err) {
+            console.log(err);
+        }else{
+            res.send("student registered succesfully!")
+        } 
+    })
+})
 
 app.listen(3001,()=>{
     console.log("Corriendo en el puerto 3001")
